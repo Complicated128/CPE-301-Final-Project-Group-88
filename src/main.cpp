ea2f205028018ee437757c80f062b2dc1909cbe2
ayayrom <hatomemes@gmail.com>
2025-05-08 22:31:57 -0700

    format

--- src/main.cpp
+++ src/main.cpp
@@ -11,115 +11,115 @@
 /**
  * D29 (PA7), D27 (PA5), D25 (PA3)
  */
-volatile unsigned char *ddra = (unsigned char*) 0x21;
-volatile unsigned char *porta = (unsigned char*) 0x22;
-volatile unsigned char *pina = (unsigned char*) 0x20;
+volatile unsigned char *ddra = (unsigned char *)0x21;
+volatile unsigned char *porta = (unsigned char *)0x22;
+volatile unsigned char *pina = (unsigned char *)0x20;
 /**
  * D52 (PB1), D53 (PB0), D12 (PB6), D13 (PB7), D10 (PB4)
  */
-volatile unsigned char *ddrb = (unsigned char*) 0x24;
-volatile unsigned char *portb = (unsigned char*) 0x25;
-volatile unsigned char *pinb = (unsigned char*) 0x23;
+volatile unsigned char *ddrb = (unsigned char *)0x24;
+volatile unsigned char *portb = (unsigned char *)0x25;
+volatile unsigned char *pinb = (unsigned char *)0x23;
 /**
  * D37 (PC0), D35 (PC2), D33 (PC4)
  */
-volatile unsigned char *ddrc = (unsigned char*) 0x27;
-volatile unsigned char *portc = (unsigned char*) 0x28;
-volatile unsigned char *pinc = (unsigned char*) 0x26;
+volatile unsigned char *ddrc = (unsigned char *)0x27;
+volatile unsigned char *portc = (unsigned char *)0x28;
+volatile unsigned char *pinc = (unsigned char *)0x26;
 /**
  * D20 (PD1), D21 (PD0)
  */
-volatile unsigned char *ddrd = (unsigned char*) 0x2A;
-volatile unsigned char *portd = (unsigned char*) 0x2B;
-volatile unsigned char *pind = (unsigned char*) 0x29;
+volatile unsigned char *ddrd = (unsigned char *)0x2A;
+volatile unsigned char *portd = (unsigned char *)0x2B;
+volatile unsigned char *pind = (unsigned char *)0x29;
 /**
  * D3 (PE5)
  */
-volatile unsigned char *ddre = (unsigned char*) 0x2D;
-volatile unsigned char *porte = (unsigned char*) 0x2E;
-volatile unsigned char *pine = (unsigned char*) 0x2C;
+volatile unsigned char *ddre = (unsigned char *)0x2D;
+volatile unsigned char *porte = (unsigned char *)0x2E;
+volatile unsigned char *pine = (unsigned char *)0x2C;
 /**
  * D54/A0 (PF0)
- */ //ANALOG
-volatile unsigned char *ddrf = (unsigned char*) 0x30;
-volatile unsigned char *portf = (unsigned char*) 0x31;
-volatile unsigned char *pinf = (unsigned char*) 0x2F;
+ */
+// ANALOG
+volatile unsigned char *ddrf = (unsigned char *)0x30;
+volatile unsigned char *portf = (unsigned char *)0x31;
+volatile unsigned char *pinf = (unsigned char *)0x2F;
 /**
  * D39 (PG2)
  */
-volatile unsigned char *ddrg = (unsigned char*) 0x33;
-volatile unsigned char *portg = (unsigned char*) 0x34;
-volatile unsigned char *ping = (unsigned char*) 0x32;
+volatile unsigned char *ddrg = (unsigned char *)0x33;
+volatile unsigned char *portg = (unsigned char *)0x34;
+volatile unsigned char *ping = (unsigned char *)0x32;
 /**
  * D9 (PH6), D8 (PH5), D7 (PH4)
  */
-volatile unsigned char *ddrh = (unsigned char*) 0x101;
-volatile unsigned char *porth = (unsigned char*) 0x102;
-volatile unsigned char *pinh = (unsigned char*) 0x100;
+volatile unsigned char *ddrh = (unsigned char *)0x101;
+volatile unsigned char *porth = (unsigned char *)0x102;
+volatile unsigned char *pinh = (unsigned char *)0x100;
 /**
  * D43 (PL6), D45 (PL4), D47 (PL2), D49 (PL0)
  */
-volatile unsigned char *ddrl = (unsigned char*) 0x10A;
-volatile unsigned char *portl = (unsigned char*) 0x10B;
-volatile unsigned char *pinl = (unsigned char*) 0x109;
+volatile unsigned char *ddrl = (unsigned char *)0x10A;
+volatile unsigned char *portl = (unsigned char *)0x10B;
+volatile unsigned char *pinl = (unsigned char *)0x109;
 
-//UART pointers
-volatile unsigned char *myUCSR0A = (unsigned char*) 0x00C0;
-volatile unsigned char *myUCSR0B = (unsigned char*) 0x00C1;
-volatile unsigned char *myUCSR0C = (unsigned char*) 0x00C2;
-volatile unsigned char *myUBRR0 = (unsigned char*) 0x00C4;
-volatile unsigned char *myUDR0 = (unsigned char*) 0x00C6;
+// UART pointers
+volatile unsigned char *myUCSR0A = (unsigned char *)0x00C0;
+volatile unsigned char *myUCSR0B = (unsigned char *)0x00C1;
+volatile unsigned char *myUCSR0C = (unsigned char *)0x00C2;
+volatile unsigned char *myUBRR0 = (unsigned char *)0x00C4;
+volatile unsigned char *myUDR0 = (unsigned char *)0x00C6;
 
-//Timer Pointers
-volatile unsigned char *myTCCR1A = (unsigned char*) 0x80;
-volatile unsigned char *myTCCR1B = (unsigned char*) 0x81;
-volatile unsigned char *myTCCR1C = (unsigned char*) 0x82;
-volatile unsigned char *myTIMSK1 = (unsigned char*) 0x6F;
-volatile unsigned char *myTIFR1 = (unsigned char*) 0x36;
-volatile unsigned int *myTCNT1 = (unsigned int*) 0x84;
+// Timer Pointers
+volatile unsigned char *myTCCR1A = (unsigned char *)0x80;
+volatile unsigned char *myTCCR1B = (unsigned char *)0x81;
+volatile unsigned char *myTCCR1C = (unsigned char *)0x82;
+volatile unsigned char *myTIMSK1 = (unsigned char *)0x6F;
+volatile unsigned char *myTIFR1 = (unsigned char *)0x36;
+volatile unsigned int *myTCNT1 = (unsigned int *)0x84;
 
-//Water sensor registers
-volatile unsigned char *myADMUX = (unsigned char*) 0x7C;
-volatile unsigned char *myADCSRB = (unsigned char*) 0x7B;
-volatile unsigned char *myADCSRA = (unsigned char*) 0x7A;
-volatile unsigned int *myADC_DATA = (unsigned int*) 0x78;
+// Water sensor registers
+volatile unsigned char *myADMUX = (unsigned char *)0x7C;
+volatile unsigned char *myADCSRB = (unsigned char *)0x7B;
+volatile unsigned char *myADCSRA = (unsigned char *)0x7A;
+volatile unsigned int *myADC_DATA = (unsigned int *)0x78;
 
 // Define pin assignments
-const int buttonInterruptPin = 3;   // Button connected to interrupt pin
-const int relay1Pin = 53;           // Fan Relay In
-const int relay2Pin = 52;           // Pump Relay In
-const int buttonLeftPin = 12;       // STPRR
-const int buttonRightPin = 13;      // STPRL
-const int led1Pin = 10;             // LED G
-const int led2Pin = 9;              // LED YL
-const int led3Pin = 8;              // LED R
-const int led4Pin = 7;              // LED B
+const int buttonInterruptPin = 3; // Button connected to interrupt pin
+const int relay1Pin = 53;         // Fan Relay In
+const int relay2Pin = 52;         // Pump Relay In
+const int buttonLeftPin = 12;     // STPRR
+const int buttonRightPin = 13;    // STPRL
+const int led1Pin = 10;           // LED G
+const int led2Pin = 9;            // LED YL
+const int led3Pin = 8;            // LED R
+const int led4Pin = 7;            // LED B
 
 // Stepper motor pins and configuration
-const int stepsPerRevolution = 2048;  // 28BYJ-48 stepper has 2048 steps per revolution
-const int stepper1Pin = 43;           // IN1
-const int stepper2Pin = 45;           // IN2
-const int stepper3Pin = 47;           // IN3
-const int stepper4Pin = 49;           // IN4
+const int stepsPerRevolution = 2048; // 28BYJ-48 stepper has 2048 steps per revolution
+const int stepper1Pin = 43;          // IN1
+const int stepper2Pin = 45;          // IN2
+const int stepper3Pin = 47;          // IN3
+const int stepper4Pin = 49;          // IN4
 // Initialize stepper library
 Stepper myStepper(stepsPerRevolution, stepper1Pin, stepper3Pin, stepper2Pin, stepper4Pin);
-
 
 // Variables to track button states
 volatile bool interruptButtonPressed = false;
 bool lastLeftButtonState = HIGH;
 bool lastRightButtonState = HIGH;
 unsigned long lastDebounceTime = 0;
-unsigned long debounceDelay = 50;  // Debounce time in milliseconds
+unsigned long debounceDelay = 50; // Debounce time in milliseconds
 
 // Initialize the liquid crystal display
 const int RS = 29, EN = 27, D4 = 33, D5 = 35, D6 = 37, D7 = 39;
 LiquidCrystal lcd(RS, EN, D4, D5, D6, D7);
 
-//Initialize RTC
+// Initialize RTC
 RTC_DS1307 rtc;
 
-//Initialize DHT
+// Initialize DHT
 DHT dht(25, DHT11);
 
 // Function prototypes
@@ -127,13 +127,13 @@ void setup_timer_regs();
 void U0Init(int); // serial port initialization
 void adc_init();
 unsigned int adc_read(unsigned char);
-ISR(TIMER1_OVF_vect);
 void printMessage(unsigned char[]); // print strings
-void putChar(unsigned char); // smaller version of printMessage
+void putChar(unsigned char);        // smaller version of printMessage
 void handleInterrupt();
 
 // States the system will be in
-enum SystemState {
+enum SystemState
+{
   DISABLED,
   IDLE,
   ERROR,
@@ -153,7 +153,8 @@ bool needClear = true;
 // Temperature Threshold = 10
 // Water Level Threshold = 320
 
-void setup() {
+void setup()
+{
   // Start the UART serial communication
   U0Init(9600);
   // Setup the ADC
@@ -168,15 +169,15 @@ void setup() {
   // Set stepper motor speed
   myStepper.setSpeed(10); // Set speed to 10 RPM
 
-  printMessage((unsigned char*)"Component Test Program\0");
+  printMessage((unsigned char *)"Component Test Program\0");
 
   // pinMode(buttonInterruptPin, INPUT); // PE5
-  *ddre &= ~(0x01 << 5); 
+  *ddre &= ~(0x01 << 5);
   // pinMode(buttonLeftPin, INPUT); // PB6
-  *ddrb &= ~(0x01 << 6); 
+  *ddrb &= ~(0x01 << 6);
   // pinMode(buttonRightPin, INPUT); // PB7
-  *ddrb &= ~(0x01 << 7); 
-  
+  *ddrb &= ~(0x01 << 7);
+
   // Set pin modes for relays and LEDs
   // pinMode(relay1Pin, OUTPUT); //D53 = PB0
   *ddrb |= (0x01 << 0);
@@ -208,111 +209,122 @@ void setup() {
 
   myStepper.setSpeed(25);
 
-  printMessage((unsigned char*)"Setup complete. Ready for testing.\0");
+  printMessage((unsigned char *)"Setup complete. Ready for testing.\0");
 }
 
-void loop() {
+void loop()
+{
   unsigned int waterThreshold = 320;
   unsigned int sensorVal = adc_read(0);
   if (interruptButtonPressed)
   {
-    printMessage((unsigned char*)"Interrupt button pressed- toggling relays\0");
+    printMessage((unsigned char *)"Interrupt button pressed- toggling relays\0");
     static bool relaysOn = false;
     relaysOn = !relaysOn;
-    //digitalWrite(relay1Pin, relaysOn);
+    // digitalWrite(relay1Pin, relaysOn);
     *portb |= (0x01 << 0);
-    //digitalWrite(relay2Pin, relaysOn);
+    // digitalWrite(relay2Pin, relaysOn);
     *portb |= (0x01 << 2);
     // Green LED (led1Pin) indicates relay currentState
-    //digitalWrite(led1Pin, relaysOn);
+    // digitalWrite(led1Pin, relaysOn);
     *portb |= (0x01 << 4);
   }
 
   // Checks what currentState the system needs to be in
-  if (temp <= 10) {
+  if (temp <= 10)
+  {
     currentState = IDLE;
-  } else if (temp > 10) {
+  }
+  else if (temp > 10)
+  {
     currentState = RUNNING;
-  } else if (currentState == ERROR) {
+  }
+  else if (currentState == ERROR)
+  {
     currentState = IDLE;
-  } else {
+  }
+  else
+  {
     currentState = DISABLED;
   }
   // Change the currentState of the system
-  switch (currentState) {
-    case DISABLED:
-      // Handle disabled currentState
-      fanOn = false;
-      displayTH = false;
-      stepperState = true;
-      waterMonitor = false;
-      // Turn on yellow LED
-      *portb &= (0x01 << 4);
-      *porth |= (0x01 << 6);
-      *porth &= (0x01 << 5);
-      *porth &= (0x01 << 4);
-      break;
-    case IDLE:
-      // Handle idle currentState
-      fanOn = false;
-      displayTH = true;
-      stepperState = true;
-      waterMonitor = true;
-      // Turn on green LED
-      *portb |= (0x01 << 4);
-      *porth &= (0x01 << 6);
-      *porth &= (0x01 << 5);
-      *porth &= (0x01 << 4);
-      break;
-    case ERROR: 
-      // Handle error currentState
-      lcd.clear();
-      lcd.print("Error: Low Water Level");
-      fanOn = false;
-      displayTH = true;
-      stepperState = false;
-      waterMonitor = true;
-      // Turn on red LED
-      *portb &= (0x01 << 4);
-      *porth &= (0x01 << 6);
-      *porth |= (0x01 << 5);
-      *porth &= (0x01 << 4);
-      break;
-    case RUNNING:
-      // Handle running currentState
-      fanOn = true;
-      displayTH = true;
-      stepperState = true;
-      waterMonitor = true;
-      // Turn on blue LED
-      *portb &= (0x01 << 4);
-      *porth &= (0x01 << 6);
-      *porth &= (0x01 << 5);
-      *porth |= (0x01 << 4);
-      break;
-    default:
-      break; 
-    }
-    // Check water level and update currentState if needed
-    if (waterMonitor) {
+  switch (currentState)
+  {
+  case DISABLED:
+    // Handle disabled currentState
+    fanOn = false;
+    displayTH = false;
+    stepperState = true;
+    waterMonitor = false;
+    // Turn on yellow LED
+    *portb &= (0x01 << 4);
+    *porth |= (0x01 << 6);
+    *porth &= (0x01 << 5);
+    *porth &= (0x01 << 4);
+    break;
+  case IDLE:
+    // Handle idle currentState
+    fanOn = false;
+    displayTH = true;
+    stepperState = true;
+    waterMonitor = true;
+    // Turn on green LED
+    *portb |= (0x01 << 4);
+    *porth &= (0x01 << 6);
+    *porth &= (0x01 << 5);
+    *porth &= (0x01 << 4);
+    break;
+  case ERROR:
+    // Handle error currentState
+    lcd.clear();
+    lcd.print("Error: Low Water Level");
+    fanOn = false;
+    displayTH = true;
+    stepperState = false;
+    waterMonitor = true;
+    // Turn on red LED
+    *portb &= (0x01 << 4);
+    *porth &= (0x01 << 6);
+    *porth |= (0x01 << 5);
+    *porth &= (0x01 << 4);
+    break;
+  case RUNNING:
+    // Handle running currentState
+    fanOn = true;
+    displayTH = true;
+    stepperState = true;
+    waterMonitor = true;
+    // Turn on blue LED
+    *portb &= (0x01 << 4);
+    *porth &= (0x01 << 6);
+    *porth &= (0x01 << 5);
+    *porth |= (0x01 << 4);
+    break;
+  default:
+    break;
+  }
+  // Check water level and update currentState if needed
+  if (sensorVal < waterThreshold)
+  {
+    currentState = ERROR;
+  }
+  // Set stepper motor speed
+  if (stepperState)
+  {
+    
+  }
+  // display temperature and humidity
+  if (displayTH)
+  {
 
+  }
 
-    }
-    // Set stepper motor speed
-    if (stepperState) {
-
-    }
-    //display temperature and humidity
-    if (displayTH) {
-
-    }
-
-    previousState = currentState;
+  previousState = currentState;
 }
 
 void setup_timer_regs()
 {
-  //TODO: dont know what to put
+  // TODO: dont know what to put
 }
 
 /* Serial port initialization
@@ -337,8 +349,8 @@ void U0Init(int U0baud)
  * ADC channel = 0
  * ADC interrupt = disabled
  * ADC trigger = disabled
- * ADC enable = enabled 
-*/
+ * ADC enable = enabled
+ */
 void adc_init()
 {
   *myADCSRA |= 0x80; // enable ADC
@@ -346,31 +358,19 @@ void adc_init()
   *myADCSRA &= 0xEF; // disable ADC interrupt
   *myADCSRA &= 0xF8; // set prescaler to 64
   *myADCSRB &= 0xF0; // clear gain bits
-  *myADMUX = 0x40; // set reference voltage to AVcc
+  *myADMUX = 0x40;   // set reference voltage to AVcc
 }
 
 unsigned int adc_read(unsigned char adc_num)
 {
-  *myADMUX &= 0xE0; // clear MUX4:0
-  *myADCSRB &= 0xDF; // clear MUX5
+  *myADMUX &= 0xE0;             // clear MUX4:0
+  *myADCSRB &= 0xDF;            // clear MUX5
   *myADMUX |= (adc_num & 0x1F); // channel 0 selection
-  *myADCSRA |= 0x40; // conversion
-  while ((*myADCSRA & 0x40) != 0);
+  *myADCSRA |= 0x40;            // conversion
+  while ((*myADCSRA & 0x40) != 0)
+    ;
   unsigned int val = *myADC_DATA;
   return val;
-}
-
-ISR(TIMER1_OVF_vect)
-{
-  // *myTCCR1B &= 0xF8;
-  // *myTCNT1 = (unsigned int)(65535 - (unsigned long)(currentTicks));
-  // *myTCCR1B |= 0x01;
-  // if (currentTicks != 65535)
-  // {
-  //   *portb ^= 0x40;
-  // }
-
-  //TODO: dunno what to put
 }
 
 void printMessage(unsigned char msg[])
@@ -383,7 +383,8 @@ void printMessage(unsigned char msg[])
 
 void putChar(unsigned char U0pdata)
 {
-  while (!(*myUCSR0A & TBE));
+  while (!(*myUCSR0A & TBE))
+    ;
   *myUDR0 = U0pdata;
 }
 
@@ -392,21 +393,24 @@ void handleInterrupt()
   interruptButtonPressed = true;
 }
 
-void displayTimeStamp(){
-  DateTime now = rtc.now(); 
+void displayTimeStamp()
+{
+  DateTime now = rtc.now();
   String date;
   date = now.toString("YYYY-MM-DD hh:mm:ss");
-  printMessage((unsigned char*)date.c_str());
+  printMessage((unsigned char *)date.c_str());
   putChar('\n');
 }
 
-void displayTempAndHum(){
-  if(needClear){
+void displayTempAndHum()
+{
+  if (needClear)
+  {
     lcd.clear();
     needClear = false;
   }
-  lcd.setCursor(0,0);
+  lcd.setCursor(0, 0);
   lcd.print("Temp: " + (String)temp + char(223) + "C");
-  lcd.setCursor(0,1);
+  lcd.setCursor(0, 1);
   lcd.print("Humidity: " + (String)hum);
 }
